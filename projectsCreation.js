49230d6682a8a5cc9b59a318ba1be2802a90a2c5
Hristova80 <hristova_80@abv.bg>
2024-02-22 11:41:02 +0100

    Add files via upload

---
+++ projectsCreation.js
@@ -0,0 +1,23 @@
+/*Изготвяне на проекти
+Напишете програма, която изчислява колко 
+часа ще са необходими на един архитект,
+ за да изготви проектите на няколко 
+ строителни обекта. Изготвянето на един проект отнема три часа.
+Вход
+От конзолата се четат 2 реда:
+1. Името на архитекта - текст
+2. Брой на проектите, които трябва да изготви 
+- цяло число в интервала [0 … 100]
+Изход
+На конзолата се отпечатва:
+· "The architect {името на архитекта} will need {необходими часове} hours to complete {брой на проектите} project/s."*/
+function projectsCreation(input) {
+    let name = input[0];
+        let project = Number(input[1]);
+        let hours = project * 3;
+
+    
+    console.log(`The architect ${name} will need ${hours} hours to complete ${project} project/s.`)
+
+}
+projectsCreation(["George ","4 "])
